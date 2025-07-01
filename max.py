ddf38e99ba4efd4e0d8159dbd70e43a80c293e22
Akshitha379 <akshitha.v.gopu@gmail.com>
2025-07-01 10:52:10 +0530

    Update 2ndmax.py
    updated code to secondmax

--- max.py
+++ max.py
@@ -1,3 +1,9 @@
-a=[100,200,300,267,600]
-p=a.sort()
-print(a[-2])
+a=[1,2,6,7,3,4,5]
+max1=max2=a[0]
+for i in a:
+    if i > max1:
+        max2 = max1
+        max1 = i
+    elif i > max2:
+        max2 = i
+print("The maximum value in the list is:", max2)  
